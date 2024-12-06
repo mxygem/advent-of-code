80bc1845cd7c25865ea14b10a21eed2f12fd7adc
mxygem <gh@nes.33mail.com>
2024-12-06 13:37:53 -0700

    day five part one

---
+++ 2024/day_five/day_five.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("day_five_input.txt")
+	if err != nil {
+		log.Fatalf("reading input: %v", err)
+	}
+
+	rules, updates, err := parseInput(string(input))
+	if err != nil {
+		log.Fatalf("parsing input: %v", err)
+	}
+
+	sum := middlePagesSum(rules, updates)
+	fmt.Printf("day one middle pages sum: %v\n", sum)
+}
+
+func parseInput(input string) (map[int][]int, [][]int, error) {
+	rulesInput := strings.Split(string(input), "\n\n")[0]
+	updatesInput := strings.Split(string(input), "\n\n")[1]
+
+	rules, err := rules(strings.Split(rulesInput, "\n"))
+	if err != nil {
+		return nil, nil, fmt.Errorf("parsing rules: %v", err)
+	}
+
+	updates, err := updates(strings.Split(updatesInput, "\n"))
+	if err != nil {
+		return nil, nil, fmt.Errorf("parsing updates: %v", err)
+	}
+
+	return rules, updates, nil
+}
+
+func rules(rulesIn []string) (map[int][]int, error) {
+	rules := make(map[int][]int, len(rulesIn))
+
+	for _, rule := range rulesIn {
+		parts := strings.Split(rule, "|")
+
+		first, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return nil, fmt.Errorf("parsing left side of rule: %v", err)
+		}
+		second, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return nil, fmt.Errorf("parsing right side of rule: %v", err)
+		}
+		rules[first] = append(rules[first], second)
+	}
+	return rules, nil
+}
+
+func updates(updatesIn []string) ([][]int, error) {
+	updates := make([][]int, 0, len(updatesIn))
+
+	for _, updateIn := range updatesIn {
+		parts := strings.Split(updateIn, ",")
+		update := make([]int, 0, len(parts))
+
+		for _, part := range parts {
+			if part == "" {
+				continue
+			}
+
+			page, err := strconv.Atoi(part)
+			if err != nil {
+				return nil, fmt.Errorf("parsing update page: %v", err)
+			}
+			update = append(update, page)
+		}
+		if len(update) == 0 {
+			continue
+		}
+
+		updates = append(updates, update)
+	}
+	return updates, nil
+}
+
+func middlePagesSum(rules map[int][]int, updates [][]int) int {
+	var sum int
+
+	for _, update := range updates {
+		fr := filterRules(update, rules)
+		if _, valid := validUpdate(update, fr); !valid {
+			continue
+		}
+
+		sum += middlePage(update)
+	}
+
+	return sum
+}
+
+func filterRules(update []int, rules map[int][]int) map[int][]int {
+	filtered := map[int][]int{}
+
+	for _, page := range update {
+		if _, ok := rules[page]; ok {
+			filtered[page] = append(filtered[page], rules[page]...)
+		}
+	}
+
+	return filtered
+}
+
+func validUpdate(update []int, rules map[int][]int) (int, bool) {
+	for key, rule := range rules {
+		if !inOrder(update, key, rule) {
+			return key, false
+		}
+	}
+
+	return 0, true
+}
+
+func inOrder(update []int, key int, rule []int) bool {
+	keyIdx := slices.Index(update, key)
+
+	for i, u := range update {
+		for _, r := range rule {
+			if u == r && i < keyIdx {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func middlePage(update []int) int {
+	return update[len(update)/2]
+}
---
+++ 2024/day_five/day_five_test.go
@@ -0,0 +1,305 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMiddlePagesSum(t *testing.T) {
+	testCases := []struct {
+		name     string
+		rules    map[int][]int
+		updates  [][]int
+		expected int
+	}{
+		{
+			name: "example",
+			rules: map[int][]int{
+				47: {53, 13, 61, 29},
+				97: {13, 61, 47, 29, 53, 75},
+				75: {29, 53, 47, 61, 13},
+				61: {13, 53, 29},
+				29: {13},
+				53: {29, 13},
+			},
+			updates: [][]int{
+				{75, 47, 61, 53, 29},
+				{97, 61, 53, 29, 13},
+				{75, 29, 13},
+				{75, 97, 47, 61, 53},
+				{61, 13, 29},
+				{97, 13, 75, 29, 47},
+			},
+			expected: 143,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := middlePagesSum(tc.rules, tc.updates)
+
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestRules(t *testing.T) {
+	testCases := []struct {
+		name        string
+		rules       []string
+		expected    map[int][]int
+		expectedErr error
+	}{
+		{
+			name: "basic",
+			rules: []string{
+				"47|53",
+			},
+			expected: map[int][]int{
+				47: {53},
+			},
+		},
+
+		{
+			name: "example",
+			rules: []string{
+				"47|53",
+				"97|13",
+				"97|61",
+				"97|47",
+				"75|29",
+				"61|13",
+				"75|53",
+				"29|13",
+				"97|29",
+				"53|29",
+				"61|53",
+				"97|53",
+				"61|29",
+				"47|13",
+				"75|47",
+				"97|75",
+				"47|61",
+				"75|61",
+				"47|29",
+				"75|13",
+				"53|13",
+			},
+			expected: map[int][]int{
+				47: {53, 13, 61, 29},
+				97: {13, 61, 47, 29, 53, 75},
+				75: {29, 53, 47, 61, 13},
+				61: {13, 53, 29},
+				29: {13},
+				53: {29, 13},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := rules(tc.rules)
+
+			if tc.expectedErr != nil {
+				assert.EqualError(t, err, tc.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestUpdates(t *testing.T) {
+	testCases := []struct {
+		name        string
+		updates     []string
+		expected    [][]int
+		expectedErr error
+	}{
+		{
+			name: "basic",
+			updates: []string{
+				"75,47,61,53,29",
+			},
+			expected: [][]int{
+				{75, 47, 61, 53, 29},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := updates(tc.updates)
+
+			if tc.expectedErr != nil {
+				assert.EqualError(t, err, tc.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestFilterRules(t *testing.T) {
+	testCases := []struct {
+		name     string
+		update   []int
+		rules    map[int][]int
+		expected map[int][]int
+	}{
+		{
+			name: "example",
+			update: []int{
+				75, 47, 61, 53, 29,
+			},
+			rules: map[int][]int{
+				47: {53, 13, 61, 29},
+				97: {13, 61, 47, 29, 53, 75},
+				75: {29, 53, 47, 61, 13},
+				61: {13, 53, 29},
+				29: {13},
+				53: {29, 13},
+			},
+			expected: map[int][]int{
+				75: {29, 53, 47, 61, 13},
+				47: {53, 13, 61, 29},
+				61: {13, 53, 29},
+				53: {29, 13},
+				29: {13},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := filterRules(tc.update, tc.rules)
+
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestValidUpdate(t *testing.T) {
+	rules := map[int][]int{
+		47: {53, 13, 61, 29},
+		97: {13, 61, 47, 29, 53, 75},
+		75: {29, 53, 47, 61, 13},
+		61: {13, 53, 29},
+		29: {13},
+		53: {29, 13},
+	}
+
+	testCases := []struct {
+		name          string
+		update        []int
+		expectedRule  int
+		expectedValid bool
+	}{
+		{
+			name:          "example one - valid",
+			update:        []int{75, 47, 61, 53, 29},
+			expectedValid: true,
+		},
+		{
+			name:          "example two - valid",
+			update:        []int{97, 61, 53, 29, 13},
+			expectedValid: true,
+		},
+		{
+			name:          "example three - valid",
+			update:        []int{75, 29, 13},
+			expectedValid: true,
+		},
+		{
+			name:          "example four - invalid",
+			update:        []int{75, 97, 47, 61, 53},
+			expectedRule:  97,
+			expectedValid: false,
+		},
+		{
+			name:          "example five - invalid",
+			update:        []int{61, 13, 29},
+			expectedRule:  29,
+			expectedValid: false,
+		},
+		{
+			name:          "example six - invalid",
+			update:        []int{97, 13, 75, 29, 47},
+			expectedRule:  75,
+			expectedValid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualRule, actualValid := validUpdate(tc.update, rules)
+
+			assert.Equal(t, tc.expectedRule, actualRule)
+			assert.Equal(t, tc.expectedValid, actualValid)
+		})
+	}
+}
+
+func TestInOrder(t *testing.T) {
+	testCases := []struct {
+		name     string
+		update   []int
+		key      int
+		rule     []int
+		expected bool
+	}{
+		{
+			name:     "example - in order",
+			update:   []int{75, 47, 61, 53, 29},
+			key:      75,
+			rule:     []int{29, 53, 47, 61, 13},
+			expected: true,
+		},
+		{
+			name:     "example - not in order",
+			update:   []int{75, 97, 47, 61, 53},
+			key:      97,
+			rule:     []int{13, 61, 47, 29, 53, 75},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, inOrder(tc.update, tc.key, tc.rule))
+		})
+	}
+}
+
+func TestMiddlePage(t *testing.T) {
+	testCases := []struct {
+		name     string
+		update   []int
+		expected int
+	}{
+		{
+			name:     "example one",
+			update:   []int{75, 47, 61, 53, 29},
+			expected: 61,
+		},
+		{
+			name:     "example two",
+			update:   []int{97, 61, 53, 29, 13},
+			expected: 53,
+		},
+		{
+			name:     "example three",
+			update:   []int{75, 29, 13},
+			expected: 29,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, middlePage(tc.update))
+		})
+	}
+}
